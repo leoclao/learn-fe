d16dedf454e1b98577400a0bc7755044c7b74ebe
leoclao <daoxuannhut@outlook.com>
2025-06-02 08:47:21 +0700

    feat: done day 1

---
+++ day-1/datatypes.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = "Hello";
+let c = true;
+let d = null;
+let e;
+
+console.log(typeof a); // number
+console.log(typeof b); // string
+console.log(typeof c); // boolean
+console.log(typeof d); // object (null is a special case in JavaScript)
+console.log(typeof e); // undefined
---
+++ day-1/index.js
@@ -0,0 +1,13 @@
+const a = 3;
+const b = 4;
+const c = 5;
+
+const name = "John";
+const age = 30;
+const myInfo = `Tôi tên là ${name}, năm nay tôi ${age} tuổi.`;
+
+let isDevFrontend;
+
+console.log("Sum:", a + b + c); // 12
+console.log(myInfo); // John
+console.log(typeof isDevFrontend); // undefined
